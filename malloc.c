e87427f20acefd70f7713e9912dd397fed90a5a6
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-16 00:52:03 +0200

    Added commentaries

--- malloc.c
+++ malloc.c
@@ -12,7 +12,12 @@ int main()
 	
 	name = malloc(32);
 	printf("Write your name : \n"); F;
-	scanf("%s", name);
+
+	scanf("%s", name); /*	We removed & because
+				name a is pointer and the value address is
+				equal to the memory that we allocated thanks to
+				malloc. Otherwise that will generate an error. 
+			   */
 	printf("Hello %s\n", name);
 	free(name);
 
