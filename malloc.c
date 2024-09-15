16e5516eb834e2395f97b35a95a9096ba22c6d26
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-16 00:46:26 +0200

    Added a variable which memory is dynamically handled thanks to malloc

--- malloc.c
+++ malloc.c
@@ -1,4 +1,21 @@
 
 /* malloc.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define F fflush(stdout)
+
+int main()
+{
+	char *name;
+	
+	name = malloc(32);
+	printf("Write your name : \n"); F;
+	scanf("%s", name);
+	printf("Hello %s\n", name);
+	free(name);
+
+	return 0;
+}
 
